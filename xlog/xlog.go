048e49a85575844edec97f58700ae768f4feba0f
seefs001 <i@seefs.me>
2024-09-27 01:02:39 +0800

    feat(xlog): add log format

--- xlog/xlog.go
+++ xlog/xlog.go
@@ -18,9 +18,19 @@ func Debug(msg string, args ...any) {
 	defaultLogger.Debug(msg, args...)
 }
 
+// Debugf logs a formatted debug message.
+func Debugf(format string, args ...any) {
+	defaultLogger.Debug(fmt.Sprintf(format, args...))
+}
+
 // Info logs an info message.
 func Info(msg string, args ...any) {
 	defaultLogger.Info(msg, args...)
+}
+
+// Infof logs a formatted info message.
+func Infof(format string, args ...any) {
+	defaultLogger.Info(fmt.Sprintf(format, args...))
 }
 
 // Warn logs a warning message.
@@ -28,9 +38,19 @@ func Warn(msg string, args ...any) {
 	defaultLogger.Warn(msg, args...)
 }
 
+// Warnf logs a formatted warning message.
+func Warnf(format string, args ...any) {
+	defaultLogger.Warn(fmt.Sprintf(format, args...))
+}
+
 // Error logs an error message.
 func Error(msg string, args ...any) {
 	defaultLogger.Error(msg, args...)
+}
+
+// Errorf logs a formatted error message.
+func Errorf(format string, args ...any) {
+	defaultLogger.Error(fmt.Sprintf(format, args...))
 }
 
 // ColorConsoleHandler implements a color console handler.
